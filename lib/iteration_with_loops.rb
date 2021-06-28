5c4564e5c3ac6fcb233ed54204d187fa0b2dddc4
jackcornblum <jcornblum@gmail.com>
2021-06-28 17:06:27 -0400

    commit tree 0f370a1d82cfb7583970cd44423c81f9ba4f261c

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,18 @@
+require "pry"
+
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  lowest_number_array = []
+  counter = 0
+  src.each do |arr|
+    lowest_number = 1000
+    arr.each do |num|
+      if num < lowest_number 
+        lowest_number = num
+      end
+    end
+    lowest_number_array.push(lowest_number)
+  end
+  lowest_number_array
 end
